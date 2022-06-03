0b8b9a24092a4764165a1e32a7e0e066c3f924c9
Thom Chiovoloni <chiovolonit@gmail.com>
2022-06-03 01:23:54 -0700

    Update testing and CI

--- src/lib.rs
+++ src/lib.rs
@@ -50,16 +50,12 @@
 //!     "It works on anything that implements PartialOrd and Debug!",
 //! );
 //! ```
-
 #![no_std]
 #![deny(missing_docs)]
 
-#[cfg(test)]
-extern crate std;
-
 // for use from within the macros.
 #[doc(hidden)]
-pub extern crate core;
+pub extern crate core as __core;
 
 /// Panics if the first expression is not strictly less than the second.
 /// Requires that the values be [comparable with `<`](core::cmp::PartialOrd).
@@ -75,30 +71,31 @@ pub extern crate core;
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     ma::assert_lt!(3, 4);
-///     ma::assert_lt!(3, 4, "With a message");
-///     ma::assert_lt!(3, 4, "With a formatted message: {}", "oh no");
-/// }
+/// ma::assert_lt!(3, 4);
+/// ma::assert_lt!(3, 4, "With a message");
+/// ma::assert_lt!(3, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! assert_lt {
-    ($left:expr, $right:expr $(,)?) => {
+    ($left:expr, $right:expr) => {
         match (&$left, &$right) {
             (left, right) => if !(left < right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left < right)`\n  left: `{:?}`,\n right: `{:?}`",
                     left, right,
                 );
             }
         }
     };
+    ($left:expr, $right:expr, ) => {
+        $crate::assert_lt!($left, $right)
+    };
     ($left:expr, $right:expr, $($msg_args:tt)+) => {
         match (&$left, &$right) {
             (left, right) => if !(left < right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left < right)`\n  left: `{:?}`,\n right: `{:?}`: {}",
-                    left, right, $crate::core::format_args!($($msg_args)+),
+                    left, right, $crate::__core::format_args!($($msg_args)+),
                 );
             }
         }
@@ -119,30 +116,31 @@ macro_rules! assert_lt {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     ma::assert_gt!(5, 3);
-///     ma::assert_gt!(5, 3, "With a message");
-///     ma::assert_gt!(5, 3, "With a formatted message: {}", "oh no");
-/// }
+/// ma::assert_gt!(5, 3);
+/// ma::assert_gt!(5, 3, "With a message");
+/// ma::assert_gt!(5, 3, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! assert_gt {
-    ($left:expr, $right:expr $(,)?) => {
+    ($left:expr, $right:expr) => {
         match (&($left), &($right)) {
             (left, right) => if !(left > right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left > right)`\n  left: `{:?}`,\n right: `{:?}`",
                     left, right,
                 );
             }
         }
     };
+    ($left:expr, $right:expr, ) => {
+        $crate::assert_gt!($left, $right)
+    };
     ($left:expr, $right:expr, $($msg_args:tt)+) => {
         match (&$left, &$right) {
             (left, right) => if !(left > right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left > right)`\n  left: `{:?}`,\n right: `{:?}`: {}",
-                    left, right, $crate::core::format_args!($($msg_args)+),
+                    left, right, $crate::__core::format_args!($($msg_args)+),
                 );
             }
         }
@@ -163,35 +161,36 @@ macro_rules! assert_gt {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     ma::assert_le!(4, 4);
-///     ma::assert_le!(4, 5);
-///     ma::assert_le!(4, 5, "With a message");
-///     ma::assert_le!(4, 4, "With a formatted message: {}", "oh no");
-/// }
+/// ma::assert_le!(4, 4);
+/// ma::assert_le!(4, 5);
+/// ma::assert_le!(4, 5, "With a message");
+/// ma::assert_le!(4, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! assert_le {
-    ($left:expr, $right:expr $(,)?) => {
+    ($left:expr, $right:expr) => {
         match (&$left, &$right) {
             (left, right) => if !(left <= right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left <= right)`\n  left: `{:?}`,\n right: `{:?}`",
                     left, right,
                 );
             }
         }
     };
-    ($left:expr, $right:expr, $($msg_args:tt)+) => ({
+    ($left:expr, $right:expr, ) => {
+        $crate::assert_le!($left, $right)
+    };
+    ($left:expr, $right:expr, $($msg_args:tt)+) => {
         match (&$left, &$right) {
             (left, right) => if !(left <= right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left <= right)`\n  left: `{:?}`,\n right: `{:?}`: {}",
-                    left, right, $crate::core::format_args!($($msg_args)+),
+                    left, right, $crate::__core::format_args!($($msg_args)+),
                 );
             }
         }
-    })
+    };
 }
 
 /// Panics if the first expression is not greater than or equal to the second.
@@ -208,31 +207,32 @@ macro_rules! assert_le {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     ma::assert_ge!(4, 4);
-///     ma::assert_ge!(4, 3);
-///     ma::assert_ge!(4, 3, "With a message");
-///     ma::assert_ge!(4, 4, "With a formatted message: {}", "oh no");
-/// }
+/// ma::assert_ge!(4, 4);
+/// ma::assert_ge!(4, 3);
+/// ma::assert_ge!(4, 3, "With a message");
+/// ma::assert_ge!(4, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! assert_ge {
-    ($left:expr, $right:expr $(,)?) => {
+    ($left:expr, $right:expr) => {
         match (&$left, &$right) {
             (left, right) => if !(left >= right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left >= right)`\n  left: `{:?}`,\n right: `{:?}`",
                     left, right,
                 );
             }
         }
     };
+    ($left:expr, $right:expr, ) => {
+        $crate::assert_ge!($left, $right)
+    };
     ($left:expr, $right:expr, $($msg_args:tt)+) => {
         match (&$left, &$right) {
             (left, right) => if !(left >= right) {
-                $crate::core::panic!(
+                $crate::__core::panic!(
                     "assertion failed: `(left >= right)`\n  left: `{:?}`,\n right: `{:?}`: {}",
-                    left, right, $crate::core::format_args!($($msg_args)+),
+                    left, right, $crate::__core::format_args!($($msg_args)+),
                 );
             }
         }
@@ -247,20 +247,18 @@ macro_rules! assert_ge {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     // These are compiled to nothing if debug_assertions are off!
-///     ma::debug_assert_lt!(3, 4);
-///     ma::debug_assert_lt!(3, 4, "With a message");
-///     ma::debug_assert_lt!(3, 4, "With a formatted message: {}", "oh no");
-/// }
+/// // These are compiled to nothing if debug_assertions are off!
+/// ma::debug_assert_lt!(3, 4);
+/// ma::debug_assert_lt!(3, 4, "With a message");
+/// ma::debug_assert_lt!(3, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! debug_assert_lt {
     ($($arg:tt)+) => {
-        if $crate::core::cfg!(debug_assertions) {
+        if $crate::__core::cfg!(debug_assertions) {
             $crate::assert_lt!($($arg)+);
         }
-    }
+    };
 }
 
 /// Same as [`assert_gt!`] in builds with debug assertions enabled, and a no-op
@@ -271,20 +269,18 @@ macro_rules! debug_assert_lt {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     // These are compiled to nothing if debug_assertions are off!
-///     ma::debug_assert_gt!(5, 3);
-///     ma::debug_assert_gt!(5, 3, "With a message");
-///     ma::debug_assert_gt!(5, 3, "With a formatted message: {}", "oh no");
-/// }
+/// // These are compiled to nothing if debug_assertions are off!
+/// ma::debug_assert_gt!(5, 3);
+/// ma::debug_assert_gt!(5, 3, "With a message");
+/// ma::debug_assert_gt!(5, 3, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! debug_assert_gt {
     ($($arg:tt)+) => {
-        if $crate::core::cfg!(debug_assertions) {
+        if $crate::__core::cfg!(debug_assertions) {
             $crate::assert_gt!($($arg)+);
         }
-    }
+    };
 }
 
 /// Same as [`assert_le!`] in builds with debug assertions enabled.
@@ -297,21 +293,19 @@ macro_rules! debug_assert_gt {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     // These are compiled to nothing if debug_assertions are off!
-///     ma::debug_assert_le!(4, 4);
-///     ma::debug_assert_le!(4, 5);
-///     ma::debug_assert_le!(4, 5, "With a message");
-///     ma::debug_assert_le!(4, 4, "With a formatted message: {}", "oh no");
-/// }
+/// // These are compiled to nothing if debug_assertions are off!
+/// ma::debug_assert_le!(4, 4);
+/// ma::debug_assert_le!(4, 5);
+/// ma::debug_assert_le!(4, 5, "With a message");
+/// ma::debug_assert_le!(4, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! debug_assert_le {
     ($($arg:tt)+) => {
-        if $crate::core::cfg!(debug_assertions) {
+        if $crate::__core::cfg!(debug_assertions) {
             $crate::assert_le!($($arg)+);
         }
-    }
+    };
 }
 
 /// Same as [`assert_ge!`] in builds with debug assertions enabled.
@@ -324,21 +318,19 @@ macro_rules! debug_assert_le {
 /// ```rust
 /// use more_asserts as ma;
 ///
-/// fn main() {
-///     // These are compiled to nothing if debug_assertions are off!
-///     ma::debug_assert_ge!(4, 4);
-///     ma::debug_assert_ge!(4, 3);
-///     ma::debug_assert_ge!(4, 3, "With a message");
-///     ma::debug_assert_ge!(4, 4, "With a formatted message: {}", "oh no");
-/// }
+/// // These are compiled to nothing if debug_assertions are off!
+/// ma::debug_assert_ge!(4, 4);
+/// ma::debug_assert_ge!(4, 3);
+/// ma::debug_assert_ge!(4, 3, "With a message");
+/// ma::debug_assert_ge!(4, 4, "With a formatted message: {}", "oh no");
 /// ```
 #[macro_export]
 macro_rules! debug_assert_ge {
     ($($arg:tt)+) => {
-        if $crate::core::cfg!(debug_assertions) {
+        if $crate::__core::cfg!(debug_assertions) {
             $crate::assert_ge!($($arg)+);
         }
-    }
+    };
 }
 
 /// Panics if reached. This is a variant of the standard library's
@@ -350,127 +342,17 @@ macro_rules! debug_assert_ge {
 /// # Example
 ///
 /// ```rust
-/// use more_asserts::debug_unreachable;
-///
-/// fn main() {
-///     // You probably wouldn't actually use this here
-///     let mut value = 0.5;
-///     if value < 0.0 {
-///         debug_unreachable!("Value out of range {}", value);
-///         value = 0.0;
-///     }
+/// let mut value = 0.5;
+/// if value < 0.0 {
+///     more_asserts::debug_unreachable!("Value out of range {}", value);
+///     value = 0.0;
 /// }
 /// ```
 #[macro_export]
 macro_rules! debug_unreachable {
     ($($arg:tt)*) => {
-        if $crate::core::cfg!(debug_assertions) {
-            $crate::core::unreachable!($($arg)*);
+        if $crate::__core::cfg!(debug_assertions) {
+            $crate::__core::unreachable!($($arg)*);
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::panic::catch_unwind;
-
-    #[derive(PartialOrd, PartialEq, Debug)]
-    enum DummyType {
-        Foo,
-        Bar,
-        Baz,
-    }
-
-    #[test]
-    fn test_assert_lt() {
-        assert_lt!(3, 4);
-        assert_lt!(4.0, 4.5);
-        assert_lt!("a string", "b string");
-        assert_lt!(
-            DummyType::Foo,
-            DummyType::Bar,
-            "Message with {}",
-            "cool formatting"
-        );
-
-        let a = &DummyType::Foo;
-        let b = &DummyType::Baz;
-        assert_lt!(a, b);
-
-        assert!(catch_unwind(|| assert_lt!(5, 3)).is_err());
-        assert!(catch_unwind(|| assert_lt!(5, 5)).is_err());
-        assert!(catch_unwind(|| assert_lt!(DummyType::Bar, DummyType::Foo)).is_err());
-    }
-
-    #[test]
-    fn test_assert_gt() {
-        assert_gt!(4, 3);
-        assert_gt!(4.5, 4.0);
-        assert_gt!("b string", "a string");
-        assert_gt!(
-            DummyType::Bar,
-            DummyType::Foo,
-            "Message with {}",
-            "cool formatting"
-        );
-
-        let a = &DummyType::Foo;
-        let b = &DummyType::Baz;
-        assert_gt!(b, a);
-
-        assert!(catch_unwind(|| assert_gt!(3, 5)).is_err());
-        assert!(catch_unwind(|| assert_gt!(5, 5)).is_err());
-        assert!(catch_unwind(|| assert_gt!(DummyType::Foo, DummyType::Bar)).is_err());
-    }
-
-    #[test]
-    fn test_assert_le() {
-        assert_le!(3, 4);
-        assert_le!(4, 4);
-        assert_le!(4.0, 4.5);
-        assert_le!("a string", "a string");
-        assert_le!("a string", "b string");
-        assert_le!(DummyType::Foo, DummyType::Bar, "Message");
-        assert_le!(
-            DummyType::Foo,
-            DummyType::Foo,
-            "Message with {}",
-            "cool formatting"
-        );
-
-        let a = &DummyType::Foo;
-        let b = &DummyType::Baz;
-        assert_le!(a, a);
-        assert_le!(a, b);
-
-        assert!(catch_unwind(|| assert_le!(5, 3)).is_err());
-        assert!(catch_unwind(|| assert_le!(DummyType::Bar, DummyType::Foo)).is_err());
-    }
-
-    #[test]
-    fn test_assert_ge() {
-        assert_ge!(4, 3);
-        assert_ge!(4, 4);
-        assert_ge!(4.5, 4.0);
-        assert_ge!(5.0, 5.0);
-        assert_ge!("a string", "a string");
-        assert_ge!("b string", "a string");
-        assert_ge!(DummyType::Bar, DummyType::Bar, "Example");
-        assert_ge!(
-            DummyType::Bar,
-            DummyType::Foo,
-            "Message with {}",
-            "cool formatting",
-        );
-
-        let a = &DummyType::Foo;
-        let b = &DummyType::Baz;
-        assert_ge!(a, a);
-        assert_ge!(b, a);
-
-        assert!(catch_unwind(|| assert_ge!(3, 5)).is_err());
-        assert!(catch_unwind(|| assert_ge!(DummyType::Foo, DummyType::Bar)).is_err());
-    }
-
-    // @@TODO: how to test the debug macros?
+    };
 }
